9802435a0317fdf290815e807b5a96878ed78c17
Michael Wetterauer <mjw0486@gmail.com>
2021-01-28 15:08:24 -0500

    Greedy Solution for smallestString O(n)-time O(n)-space

--- smallestString.ts
+++ smallestString.ts
@@ -13,24 +13,67 @@
 // Explanation: The numeric value of the string is 1 + 1 + 25 = 27, and it is the smallest string with such a value and length equal to 3.
 
 
-function smallestString(n : number, k : number) : string {
+function smallestStringFinder(n : number, k : number) : string {
   // create and empty string
-  // create a currentNumericVal = k
+  let smallestString : string = '';
   // create a alphaNumericMap
+  const alphaNumericMap = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h',
+    9: 'i',
+    10: 'j',
+    11: 'k',
+    12: 'l',
+    13: 'm',
+    14: 'n',
+    15: 'o',
+    16: 'p',
+    17: 'q',
+    18: 'r',
+    19: 's',
+    20: 't',
+    21: 'u',
+    22: 'v',
+    23: 'w',
+    24: 'x',
+    25: 'y',
+    26: 'z'
+  }
+  // create a currentNumericVal = k
+  let currentNumericValue : number = k;
 
   // while string length is less than n
+  while (smallestString.length < n) {
     // currentCharTarget = currentNumericVal - (n - string.length) + 1
+    const currentCharTarget : number = currentNumericValue - (n - smallestString.length) + 1;
     // if currentCharTarget >= 26
+    if (currentCharTarget >= 26) {
       // concat z with string
+      smallestString = 'z' + smallestString;
       // subtract 26 from currentNumericVal
-    // else
+      currentNumericValue -= 26;
+      // else
+    } else {
       // currentChar = alphaNumericMap[currentCharTarget];
+      const currentChar : string = alphaNumericMap[currentCharTarget];
       // subtract currentCharTarget from currentNumericVal
+      currentNumericValue -= currentCharTarget;
       // concat currentChar with string
-
+      smallestString = currentChar + smallestString;
+    }
+  }
   // return string
+  return smallestString;
 }
 
+// console.log(smallestStringFinder(1, 24)) // => 'x'
+// console.log(smallestStringFinder(4, 30)) // => 'aabz'
 
 
 
